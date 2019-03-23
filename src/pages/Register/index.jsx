cf14c050b69122c3b224cb3dec28843768352681
Tri Luong <lxt0094@gmail.com>
2019-03-23 21:39:41 +0700

    Update function for RegisterPage.
    Implement and Add function onChange and onSubmit for
    Register Pages.
    
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import { HomeContext } from '../Home'
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { HomeContext } from "../Home";
 import data from "../../dataProductList.json";
 
-const Provider = HomeContext.Provider
+const Provider = HomeContext.Provider;
 
 function RegisterPage() {
   // const [state, setState] = useState({})
@@ -12,18 +12,36 @@ function RegisterPage() {
   const [product, setProduct] = useState([]);
   const [countProduct, setCountProduct] = useState(0);
   const [dataSort, setDataSort] = useState(data);
-  
+  const [RegisterInfo, setRegisterInfo] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+
+  const ChangeHandler = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setRegisterInfo({...RegisterInfo, [name]:value});
+  }
+
+  const SubmitHandler = event => {
+    event.preventDefault();
+    console.log(RegisterInfo);
+  }
   return (
-    <Provider value={{
-      dataSort,
-      setDataSort,
-      total,
-      setTotal,
-      product,
-      setProduct,
-      countProduct,
-      setCountProduct
-    }}>
+    <Provider
+      value={{
+        dataSort,
+        setDataSort,
+        total,
+        setTotal,
+        product,
+        setProduct,
+        countProduct,
+        setCountProduct
+      }}
+    >
       <Header />
       <section className="login-area pt-100 pb-100">
         <div className="container">
@@ -31,16 +49,44 @@ function RegisterPage() {
             <div className="col-lg-8 offset-lg-2">
               <div className="basic-login">
                 <h3 className="text-center mb-60">Signup From</h3>
-                <form action="#">
-                  <label htmlFor="name">Username <span>**</span></label>
-                  <input id="name" type="text" placeholder="Enter Username or Email address..." />
-                  <label htmlFor="email-id">Email Address <span>**</span></label>
-                  <input id="email-id" type="text" placeholder="Enter Username or Email address..." />
-                  <label htmlFor="pass">Password <span>**</span></label>
-                  <input id="pass" type="password" placeholder="Enter password..." />
-                  <div className="mt-10"></div>
-                  <button className="btn theme-btn-2 w-100">Register Now</button>
-                  <div className="or-divide"><span>or</span></div>
+                <form action="#" onSubmit={SubmitHandler}>
+                  <label htmlFor="name">
+                    Username <span>**</span>
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    placeholder="Enter Username or Email address..."
+                    name="username"
+                    onChange={ChangeHandler}
+                  />
+                  <label htmlFor="email-id">
+                    Email Address <span>**</span>
+                  </label>
+                  <input
+                    id="email-id"
+                    type="text"
+                    placeholder="Enter Username or Email address..."
+                    name="email"
+                    onChange={ChangeHandler}
+                  />
+                  <label htmlFor="pass">
+                    Password <span>**</span>
+                  </label>
+                  <input
+                    id="pass"
+                    type="password"
+                    placeholder="Enter password..."
+                    name="password"
+                    onChange={ChangeHandler}
+                  />
+                  <div className="mt-10" />
+                  <button className="btn theme-btn-2 w-100" type="submit">
+                    Register Now
+                  </button>
+                  <div className="or-divide">
+                    <span>or</span>
+                  </div>
                   <button className="btn theme-btn w-100">login Now</button>
                 </form>
               </div>
